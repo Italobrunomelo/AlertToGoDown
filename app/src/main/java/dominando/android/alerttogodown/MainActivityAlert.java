f4ddecc6debbc7866064de8e0b430e6a07b4e2a6
Italobrunomelo <italobrunomelo@hotmail.com>
2017-05-11 01:26:46 -0300

    activity Create Alert

---
+++ app/src/main/java/dominando/android/alerttogodown/MainActivityAlert.java
@@ -0,0 +1,13 @@
+package dominando.android.alerttogodown;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivityAlert extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_alert);
+    }
+}
